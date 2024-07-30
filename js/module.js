fc6e77241ba478f6203b6abe63c8103f3b494fbe
SansLimited <dedesusanto1612@gmail.com>
2024-07-30 20:59:34 +0700

    Update module.js

--- js/module.js
+++ js/module.js
@@ -1,6 +1,6 @@
-import { endpoint, formH, getUrl, timeNow } from './utils.js';
+import { endpoint, formH } from './utils.js';
 
-async function getIdFromUserId(userId) {
+async function getNicknameFromUserId(userId) {
   // Implementasikan logika untuk mendapatkan nickname dari userId di sini
   // Misalnya, bisa menggunakan API pihak ketiga atau database
   // Contoh return nickname statis untuk tujuan demonstrasi
@@ -8,26 +8,34 @@ async function getIdFromUserId(userId) {
 }
 
 export default async function ff(userId) {
-  // Dapatkan nickname dari userId
-  const nickname = await getIdFromUserId(userId);
-  if (!nickname) {
-    return { success: false, message: 'Nickname tidak ditemukan untuk ID tersebut.' };
+  try {
+    // Dapatkan nickname dari userId
+    const nickname = await getNicknameFromUserId(userId);
+    if (!nickname) {
+      return { success: false, message: 'Nickname tidak ditemukan untuk ID tersebut.' };
+    }
+
+    const body = `voucherPricePoint.id=8050&voucherPricePoint.price=1000&voucherPricePoint.variablePrice=0&user.userId=${userId}&voucherTypeName=FREEFIRE&shopLang=id_ID&voucherTypeId=1&gvtId=1`;
+
+    const response = await fetch(endpoint, {
+      method: 'POST',
+      headers: formH,
+      body
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok.');
+    }
+
+    const data = await response.json();
+
+    return {
+      success: true,
+      game: 'Garena Free Fire',
+      id: Number(userId),
+      name: data.confirmationFields.roles[0].role
+    };
+  } catch (error) {
+    return { success: false, message: error.message };
   }
-
-  const body = `voucherPricePoint.id=8050&voucherPricePoint.price=1000&voucherPricePoint.variablePrice=0&user.userId=${userId}&voucherTypeName=FREEFIRE&shopLang=id_ID&voucherTypeId=1&gvtId=1`;
-
-  const response = await fetch(endpoint, {
-    method: 'POST',
-    headers: formH,
-    body
-  });
-
-  const data = await response.json();
-
-  return {
-    success: true,
-    game: 'Garena Free Fire',
-    id: Number(userId),
-    name: data.confirmationFields.roles[0].role
-  };
 }
